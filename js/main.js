5d8492c68223f11340476bc4d3b450d9f4b08394
Stefano De Capitani <stefano.deca@live.com>
2021-10-15 16:45:05 +0200

    Correzione finale

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ Si potrebbe fare saltando un if o comunque accorpando porzioni di codice.
 Occhio solo a non saltare troppi passaggi logici che per qualcuno sono ostici. */
 
 //Chiedo all'utente di inerire un numero e assegno l'entry alla variabile omonima
-var entry = prompt("inserisci un numero:");
+const entry = prompt("inserisci un numero:");
 console.log("entry: " + entry);
 //Faccio il casting dell'entry in un numero
-var number = parseInt(entry);
+let number = parseInt(entry);
 console.log("number: " + number);
 //Verifico che il number ottenuto non sia un Nan
 console.log("number is Nan?: " + isNaN(number));
@@ -18,9 +18,11 @@ if (!isNaN(number)) {
   //Se ePari
   if (ePari) {
     //Stampo il numero
+    console.log("Stampo numero pari " + number);
     alert(number);
   } else {
     //Stampo il numero successivo
+    console.log("Stampo numero dispari " + (number + 1));
     alert(number + 1);
   }
 }
