f79a30626a4cf8aaf0c6c8553355213d72424bc4
Stefano De Capitani <stefano.deca@live.com>
2021-10-15 17:00:12 +0200

    Snack 2 terminato

---
+++ js/snack2.js
@@ -0,0 +1,35 @@
+/* Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi all’array che ha meno elementi
+fino a quando ne avrà tanti quanti l’altro. */
+
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+//imposto un ciclo che si interromperà quando i due array avranno la stessa lunghezza
+console.log(
+  "I due array hanno la stessa lunghezza?: " + array1.lenght != array2.lenght
+);
+while (array1.length != array2.length) {
+  //genero un nuovo elemento da aggiungere all'array deficitario
+  let newElement = Math.round(Math.random() * 100);
+  console.log(
+    "Nuovo elemento da inserire nell'array deficitarion: " + newElement
+  );
+  //Imposto una condizione per definire quale dei due array ha un numero maggiore di elementi
+  if (array1.length < array2.lenght) {
+    //Aggiungo l'elemento generato all'array deficitario
+    console.log("Inserisco il nuovo elemento nell'array 1");
+    array1.push(newElement);
+    console.log("Array 1 aggiornato: " + array1);
+  } else {
+    //Aggiungo l'elemento generato all'array deficitario
+    console.log("Inserisco il nuovo elemento nell'array 2");
+    array2.push(newElement);
+    console.log("Array 2 aggiornato: " + array2);
+  }
+}
+
+//Stampo a console il risultato del ciclo
+console.log("Ciclo terminato i due array hanno la stessa lunghezza: ");
+console.log(array1);
+console.log(array2);
